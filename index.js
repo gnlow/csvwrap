b6e40d42bf493da06adaacfc96a008ca91ea0530
Gnlow <entrydark@gmail.com>
2020-04-30 10:43:12 +0900

    add parser function

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { program } = require("commander")
 
+const parse = csv => {
+    let items = data.split("\n")
+    let label = items.splice(0, 1)[0].split(",").map(x => x.trim())
+    return items.map(item => 
+        item.split(",")
+            .reduce( (data, current, i) => {
+                data[label[i]] = current.trim()
+                return data
+            }, {} )
+    )
+}
+
 module.exports = () => {
     program
-        .version('0.0.1')
-        .action(x=>console.log("Hello"))
+        .option("-x, --xx [word]", "X")
+
         .parse(process.argv)
+        console.log(program.xx)
 }
