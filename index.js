5b692ecfd200cb992980c3cf66f57ea5684061bc
Gnlow <entrydark@gmail.com>
2020-05-24 11:37:05 +0900

    add numberize option

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const util = require("util")
 const read = util.promisify(fs.readFile)
 const write = util.promisify(fs.writeFile)
 
-const parse = csv => {
+const parse = (csv, numberize) => {
     let items = csv.trim().split("\n")
     let label = items.splice(0, 1)[0].split(",").map(x => x.trim())
-    return {label, data: items.map(item => item.split(",").map(x => x.trim()))}
+    return {label, data: items.map(
+        item => 
+            item.split(",")
+                .map(
+                    x => {
+                        const data = x.trim()
+                        return numberize 
+                            ? (isNaN(data) ? data : Number(data)) 
+                            : data
+                    }
+                )
+    )}
 }
 
 const target = {
@@ -21,13 +32,14 @@ module.exports = async () => {
     program
         .version("0.2.0")
         .option("-t, --target <target>", "module export type (require or es)")
+        .option("-n, --numberize", "numberize data")
         .parse(process.argv)
 
         const files = await globby("**/*.csv")
         const data = await Promise.all(files.map(file => read(file, "utf-8")))
         data.forEach((datum, i) => {
             write(files[i].replace(/(.*)\.csv/, "$1.js"), 
-                target[program.target](JSON.stringify(parse(datum)))
+                target[program.target](JSON.stringify(parse(datum, program.numberize)))
             )
         } )
 }
